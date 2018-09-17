b81ef75e4fc74f12362f12cfc3217427584f1de0
Luke Berry <luke.berry@my.jcu.edu.au>
2018-09-17 22:39:42 +1000

    guitar class and test it

---
+++ prac_06/Guitars.py
@@ -0,0 +1,16 @@
+class Guitars:
+    def __init__(self, name, year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def str(self):
+        return "{} ({}) : {}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        age = 2018 - self.year
+        return "The {} is 2018 - {} = {}".format(self.name, self.year, age)
+
+    def is_vintage(self, age):
+        if age >= 50:
+            return "{} is vintage.format(self.name)"
